686ec9968208901277d9e02b591bd91c0091eca2
zeina101 <zeina.syedain@cas.edu.pk>
2025-04-24 20:47:33 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-#open file and store file object in a variable.
-file = open("text.txt")
+# open the file and read mode
 
-#read the contents of the file
-print(file.read())
+file_read = open("penguins.txt")
+print("File in read mode-")
+print(file_read.read())
+file_read.close()
 
-#close the file
-file.close()
+# open the file in write mode
+file_write = open("penguins.txt", "w")
+# write in the file
+file_write.write("File in the write mode-")
+file_write.write("some dude with a border crisis on his face has beef with penguins somehow.")
+file_write.close()
 
+# open the file in append mode
+file_append = open("penguins.txt")
+#append in the file
+file_append.write("\n File in append mode-")
+file_append.write("some dude with a border crisis on his face has beef with penguins somehow.")
+file_append.close()
